4af49ed989408b3e1163b92543eeaa1806a7909d
Peilin Yang <yangpeilyn@gmail.com>
2016-09-19 23:34:44 -0400

    format correction

--- performance_analysis/plot_cost.py
+++ performance_analysis/plot_cost.py
@@ -23,7 +23,7 @@ class PerformaceAnalysis(object):
     def __init__(self):
         pass
 
-    def gen_performance_trending(self, collection='../../../reproduce/collections/wt2g'):
+    def gen_performance_trending(self, collection='../../../reproduce/collections/gov2'):
         """
         Generate the performance trendings based on the parameter of 
         the model and the number of terms in the query
