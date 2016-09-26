749818076facccd56483d79570dbd5270a217b1d
Peilin Yang <yangpeilyn@gmail.com>
2016-09-26 11:27:48 -0400

    refactor plots

--- performance_analysis/analysis.py
+++ performance_analysis/analysis.py
@@ -42,6 +42,7 @@ class PerformaceAnalysis(object):
         query_instance = Query(collection)
         eval_instance = Evaluation(collection)
         query_nums = query_instance.get_queries_lengths(query_part)
+        print query_nums
         #plot related
         num_cols = 2
         num_rows = int(math.ceil(len(query_nums)*1.0/num_cols))
@@ -53,6 +54,7 @@ class PerformaceAnalysis(object):
         col_idx = 0
         for i in query_nums:
             qids = [q['num'] for q in query_instance.get_queries_of_length(i, query_part)]
+            print i, qids
             # we assume that the model parameters can be normalized to [0, 1]
             ax = axs[row_idx][col_idx]
             col_idx += 1
@@ -70,7 +72,7 @@ class PerformaceAnalysis(object):
                             avg_perform.append( np.mean([v[metric] for v in eval_instance.get_all_performance_of_some_queries(method = method_str, qids = qids, return_all_metrics = False).values()]) )
                         except:
                             avg_perform.append(0.0)
-                    ax.plot(x, avg_perform, markers[model_idx], label=model)
+                    ax.plot(x, avg_perform, markers[model_idx], ls='-', label=model.iterkeys.next())
             ax.set_title('qLen=%d' % i)
         plt.legend()
         plt.savefig(os.path.join(self.output_root, 
