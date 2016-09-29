24f0a563c9d64bb7c4e964c916c4ea6efe6e29e6
Peilin Yang <yangpeilyn@gmail.com>
2016-09-29 13:27:41 -0400

    refactor the code so that the plot_tf_rel is separated from the main code

--- sqa/plot_tf_rel.py
+++ sqa/plot_tf_rel.py
@@ -408,7 +408,7 @@ class PlotTFRel(SingleQueryAnalysis):
             self.plot_single_tfc_constraints_draw_pdf_line(axs, xaxis, 
                 yaxis, collection_name, 
                 collection_legend, 
-                legend_pos='best'
+                legend_pos='best',
                 xlog=False,
                 ylog=False)
             # only if we want to draw the fitting curve
@@ -420,7 +420,7 @@ class PlotTFRel(SingleQueryAnalysis):
             self.plot_single_tfc_constraints_draw_pdf(axs, xaxis, 
                 yaxis, collection_name, 
                 collection_legend, 
-                legend_pos='best'
+                legend_pos='best',
                 xlog=False,
                 ylog=False)
 
