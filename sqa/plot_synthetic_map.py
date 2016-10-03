a8015c0b6c10684bebbd704e7f2199cc1dd494cb
Peilin Yang <yangpeilyn@gmail.com>
2016-10-03 11:10:17 -0400

    add coments to plot synthetic

--- sqa/plot_synthetic_map.py
+++ sqa/plot_synthetic_map.py
@@ -32,6 +32,17 @@ class PlotSyntheticMAP(SingleQueryAnalysis):
             os.makedirs(self.output_root)
 
     def cal_map(self, ranking_list, has_total_rel=False, total_rel=0, type=1):
+        """
+        Calculate the MAP based on the ranking_list.
+
+        Input:
+        @ranking_list: The format of the ranking_list is:
+            [(num_rel_docs, num_total_docs), (num_rel_docs, num_total_docs), ...]
+            where the index corresponds to the TF, e.g. ranking_list[1] is TF=1
+        @type: 1 for best performance (relevant documents are ranked before 
+            non-relevant documents for each TF) and 0 for worst performance (
+            relevant documents are ranked after non-relevant documents for each TF)
+        """
         cur_rel = 0
         s = 0.0
         total = 0
@@ -92,38 +103,30 @@ class PlotSyntheticMAP(SingleQueryAnalysis):
 
     def plot_line(self, ax, xaxis, yaxis, 
             title="", legend="", legend_outside=False, marker=None, linestyle=None, 
-            legend_pos='best',  xlabel_format=0):
+            legend_pos='best', xlabel_format=0):
         ax.plot(xaxis, yaxis, marker=marker if marker else '+', ls=linestyle if linestyle else '-', label=legend)
         ax.set_title(title)
         ax.legend(loc=legend_pos)
         if xlabel_format != 0:
             ax.ticklabel_format(axis='x', style='sci', scilimits=(0,0))
 
-    def plot(self, plot_ratio=True, 
-            performance_as_legend=True,  drawline=True, plotbins=True, numbins=60, 
-            oformat='png'):
+    def plot(self, maxTF=20, scale_factor=1, plot_ratio=True, 
+            performance_as_legend=True, drawline=True, oformat='png'):
         """
-        plot the P(D=1|TF=x)
+        plot the relationship between P(D=1|TF=x) and the performance 
+        on a synthetic data set
 
         Input:
-        @x_func: how to get the x-axis of the figure. By default, this should 
-            be TF values. But we are flexible with other options, e.g. tf/dl
-        @_method: Which method is going to be plot. The parameters should also be 
-            attached, e.g. dir,mu:2500
         @plot_ratio: When this is false, plot the y-axis as the number of relevant 
             documents; When this is true, plot the y-axis as the #rel_docs/#docs
         @performance_as_legend: whether to add performance(e.g. MAP) 
             as part of the legend
         @drawline: draw the data points as line(true) or dots(false)
-        @plotbins: whether to group the x points as bins
-        @numbins: the number of bins if we choose to plot x points as bins
         @oformat: output format, eps or png
         """
         fig, axs = plt.subplots(nrows=1, ncols=1, sharex=False, sharey=False, figsize=(6, 3.*1))
         font = {'size' : 8}
         plt.rc('font', **font)
-        scale_factor = 1
-        maxTF = 50
         markers = ['', '+', '+', '.', 'o']
         for plot_type in range(1, 5):
             xaxis = range(1, maxTF+1)
