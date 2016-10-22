61c169a07d1dccb23a5dc8ab94b858f321d7e969
Peilin-Yang <yangpeilyn@gmail.com>
2016-10-22 09:21:50 -0400

    improve the DP

--- sqa/plot_synthetic_map.py
+++ sqa/plot_synthetic_map.py
@@ -38,18 +38,14 @@ class PlotSyntheticMAP(SingleQueryAnalysis):
         if r == 0:
            return 0
         R = {}
-        #for i in range(r+1):
-        #    for j in range(n+1):
-        #        for k in range(n+1):
-        #            R[(pr+i, pn+j, 0, k)] = 0
         for i in range(1, r+1):
             for j in range(n+1):
                 R[(pr+r-i, pn+n-j, i, 0)] = (pr+r-i+1.0) / (pr+r-i+pn+n-j+1.0) 
-                if i-1 != 0:
+                if i != 1:
                     R[(pr+r-i, pn+n-j, i, 0)] += R[(pr+r-i+1, pn+n-j, i-1, 0)]
         for i in range(1, r+1):
             for j in range(1, n+1):
-                subR = R[(pr+r-i+1, pn+n-j, i-1, j)] if i-1!=0 else 0
+                subR = R[(pr+r-i+1, pn+n-j, i-1, j)] if i!=1 else 0
                 prob_r = i*1.0/(i+j)*((pr+r-i+1.0)/(pr+r-i+pn+n-j+1)+subR) 
                 prob_n = j*1.0/(i+j)*R[(pr+r-i, pn+n-j+1, i, j-1)]
                 R[(pr+r-i, pn+n-j, i, j)] = prob_r + prob_n
