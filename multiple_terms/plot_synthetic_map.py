fd33cda79e8ed3ab3ce8761322f0271c891ed845
Peilin Yang <yangpeilyn@gmail.com>
2016-11-01 14:34:38 -0400

    MAP %.4f

--- multiple_terms/plot_synthetic_map.py
+++ multiple_terms/plot_synthetic_map.py
@@ -306,10 +306,10 @@ class PlotSyntheticMAP(object):
                 docs_cnt_scale.append(max(fit_larger_maxTF_cnt/fit_larger_maxTF_rel_cnt, 1))
         ranking_list = zip(tf_scale, docs_cnt_scale)
         print ranking_list
-        performance = '$MAP_B$:' + str(round(self.cal_map(ranking_list, type=1), 4))
-        performance += '\n$MAP_W$:' + str(round(self.cal_map(ranking_list, type=0), 4))
+        performance = '$MAP_B$:%.4f' % self.cal_map(ranking_list, type=1)
+        performance += '\n$MAP_W$:%.4f' % self.cal_map(ranking_list, type=0)
         if cal_expect_map:
-            performance += '\n$MAP_E$:' + str(round(self.cal_expected_map(ranking_list), 4))
+            performance += '\n$MAP_E$:%.4f' % self.cal_expected_map(ranking_list)
         output_fn = os.path.join(self.output_root, verbose+'.'+oformat) 
         yaxis = [ele[0]*1.0/ele[1] if ele[1] !=0 else 0 for ele in ranking_list]
         self.plot_interpolation(ranges, yaxis, title, performance, output_fn, oformat)
