d1d0534d94ddd94ee4ef1accec93d085c446f51f
Peilin Yang <yangpeilyn@gmail.com>
2016-11-11 11:11:56 -0500

    remove zero points when fitting

--- explanatory/plot_prob_rel.py
+++ explanatory/plot_prob_rel.py
@@ -307,7 +307,7 @@ class PlotRelProb(object):
                     pass
             all_fittings.sort(key=itemgetter(4))
             print all_fittings[0][0], all_fittings[0][1], all_fittings[0][2], all_fittings[0][4]
-            fitted_y = []
+            fitted_y = [0 for i in range(len(xaxis))]
             for x in xaxis:
                 if x in fitting_xaxis:
                     idx = fitting_xaxis.index(x)
