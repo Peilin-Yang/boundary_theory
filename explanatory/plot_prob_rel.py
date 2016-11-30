72b279f331407c9670c49eee7c0f47ba8e8b89df
Peilin Yang <yangpeilyn@gmail.com>
2016-11-30 13:48:25 -0500

    update x_func

--- explanatory/plot_prob_rel.py
+++ explanatory/plot_prob_rel.py
@@ -188,7 +188,7 @@ class PlotRelProb(object):
             scores = x_func(cs, tfs, doclens)
             scores = np.around(scores, decimals=4)
             scores = np.sort(scores)
-            scores = scores[::-1]
+            scores = scores[::-1][:1000]
             #print qid, scores
             decending_ranking_list = zip(scores, rels)
             # for row in doc_details.get_qid_details(qid):
