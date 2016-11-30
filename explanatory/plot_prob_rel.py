a1005ce8299124dde48adc85e6f499d806a8453c
Peilin Yang <yangpeilyn@gmail.com>
2016-11-30 13:22:43 -0500

    update x_func

--- explanatory/plot_prob_rel.py
+++ explanatory/plot_prob_rel.py
@@ -238,6 +238,7 @@ class PlotRelProb(object):
             else:
                 yaxis = [(x_dict[x][0]) if plot_rel_or_all else (x_dict[x][1]) for x in xaxis]
             ranking_list = [(x_dict[x][0], x_dict[x][1]) for x in xaxis]
+            print ranking_list
             all_expected_maps.append(EMAP().cal_expected_map(ranking_list))
 
             if draw_individual:
