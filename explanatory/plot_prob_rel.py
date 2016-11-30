b848243af5f73d751122e9dcd6b864c73a85ef21
Peilin Yang <yangpeilyn@gmail.com>
2016-11-30 13:28:14 -0500

    update x_func

--- explanatory/plot_prob_rel.py
+++ explanatory/plot_prob_rel.py
@@ -252,6 +252,7 @@ class PlotRelProb(object):
                 if curve_fitting and not plot_ratio:
                     sum_yaxis = np.sum(yaxis)
                     yaxis /= sum_yaxis
+                print xaxis, yaxis
                 query_stat = cs.get_term_stats(query_term)
                 zoom_xaxis = xaxis[zoom_x:]
                 zoom_yaxis = yaxis[zoom_x:]
