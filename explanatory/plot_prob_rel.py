8bb6b06f8e2d86fd131fbaf52c8aa9cf812e0d50
Peilin Yang <yangpeilyn@gmail.com>
2016-12-01 09:43:41 -0500

    update x_func

--- explanatory/plot_prob_rel.py
+++ explanatory/plot_prob_rel.py
@@ -191,8 +191,8 @@ class PlotRelProb(object):
             scores = np.around(scores, decimals=4)
             scores = np.sort(scores)
             scores = scores[::-1][:1000]
-            print scores
             decending_ranking_list = zip(scores, rels)
+            print decending_ranking_list
             # for row in doc_details.get_qid_details(qid):
             #     x = x_func(cs, row)
             #     if x > collection_level_maxX:
