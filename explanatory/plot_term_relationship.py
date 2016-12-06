7b20060ad33174f1848dfc42ecd29795748ddbb1
Peilin Yang <yangpeilyn@gmail.com>
2016-12-06 15:21:37 -0500

    add plots

--- explanatory/plot_term_relationship.py
+++ explanatory/plot_term_relationship.py
@@ -183,10 +183,8 @@ class PlotTermRelationship(object):
             'avg TF ratio',
             'avg of all terms',
             'inner doc TF diff == 0',
-            'inner doc TF diff <= 1',
-            'inner doc TF diff <= 3',
-            'inner doc TF diff <= 5',
-            'inner doc TF diff <= 10',
+            'inner doc TF diff [1,3]',
+            'inner doc TF diff [3.10]',
             'inner doc TF diff > 10',
         ]
         data = []
@@ -204,10 +202,8 @@ class PlotTermRelationship(object):
                 np.mean(tfs, axis=0)[np.argmin(dfs)] / col_means[np.argmax(dfs)], # ratio
                 np.mean(tfs), # all counts avg
                 np.count_nonzero(abs_row_diffs == 0)*1. / abs_row_diffs.size,  
-                np.count_nonzero(abs_row_diffs <= 1)*1. / abs_row_diffs.size, 
-                np.count_nonzero(abs_row_diffs <= 3)*1. / abs_row_diffs.size, 
-                np.count_nonzero(abs_row_diffs <= 5)*1. / abs_row_diffs.size, 
-                np.count_nonzero(abs_row_diffs <= 10)*1. / abs_row_diffs.size, 
+                np.count_nonzero(abs_row_diffs >= 1 and abs_row_diffs <= 3)*1. / abs_row_diffs.size, 
+                np.count_nonzero(abs_row_diffs > 3 and abs_row_diffs <= 10)*1. / abs_row_diffs.size, 
                 np.count_nonzero(abs_row_diffs > 10)*1. / abs_row_diffs.size, 
             ])
         return all_labels, np.array(data).transpose()
