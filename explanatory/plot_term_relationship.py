4f1f2dfba90de1ce2da7c7b3a7015494ce5a2d3a
Peilin Yang <yangpeilyn@gmail.com>
2016-12-06 14:26:13 -0500

    add plots

--- explanatory/plot_term_relationship.py
+++ explanatory/plot_term_relationship.py
@@ -182,6 +182,7 @@ class PlotTermRelationship(object):
             'avg TF ratio',
             'avg of all terms',
         ]
+        print all_tfs
         data = [[
             np.mean(tfs, axis=0)[np.argmax(details_data[qid][2])], # avg TF of terms with smaller IDF
             np.mean(tfs, axis=0)[np.argmin(details_data[qid][2])], # avg TF of terms with larger IDF
