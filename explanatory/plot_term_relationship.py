cea9c97e19965c8764dea612781b77cb50cd2fa4
Peilin Yang <yangpeilyn@gmail.com>
2016-12-05 22:22:25 -0500

    add plots

--- explanatory/plot_term_relationship.py
+++ explanatory/plot_term_relationship.py
@@ -115,7 +115,7 @@ class PlotTermRelationship(object):
                 if i not in countings[qid]:
                     countings[qid][i] = {'cnt': 0, 'rel_ratio': 0, 'total_ratio': 0}
             if 0 not in countings[qid]:
-                cnt = rel_data[qid]['rel_cnt'] - sum([countings[qid][v]['cnt'] for v in countings[qid]])
+                cnt = rel_data[qid]['rel_cnt'] - sum([countings[qid][v]['cnt'] for v in countings[qid]]) if qid in rel_data else 0
                 countings[qid][0] = {'cnt': cnt, 'rel_ratio':cnt*1./rel_data[qid]['rel_cnt'], 'total_ratio': 0}
         return countings
 
