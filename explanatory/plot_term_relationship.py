6fe49fb6646db0b531a3d6917764f6388ea97ac3
Peilin Yang <yangpeilyn@gmail.com>
2016-12-06 13:51:13 -0500

    add plots

--- explanatory/plot_term_relationship.py
+++ explanatory/plot_term_relationship.py
@@ -169,15 +169,15 @@ class PlotTermRelationship(object):
         """
         for qid, tfs in all_tfs.items():
             print qid, tfs, details_data[qid][2], np.argmax(details_data[qid][2]), np.argmin(details_data[qid][2])
-            print np.mean(tfs[np.argmax(details_data[qid][2])])
-            print np.mean(tfs[np.argmin(details_data[qid][2])])
+            print np.mean(tfs, axis=0)[np.argmax(details_data[qid][2])]
+            print np.mean(tfs, axis=0)[np.argmin(details_data[qid][2])]
             print np.mean(np.mean(tfs, axis=0))
             print np.fabs(np.diff(np.mean(tfs, axis=0)))
             print np.mean(np.mean(tfs, axis=1))
             raw_input()
         data = [[
             np.mean(tfs[np.argmax(details_data[qid][2])]), # avg TF of terms with smaller IDF
-            np.mean(tfs[np.argmin(details_data[qid][2])]), # avg TF of terms with smaller IDF
+            np.mean(tfs[np.argmin(details_data[qid][2])]), # avg TF of terms with larger IDF
             np.mean(np.mean(tfs, axis=0)), # avg of two avg TF
             np.fabs(np.diff(np.mean(tfs, axis=0))), # diff of two avg TF
             np.mean(np.mean(tfs, axis=1)), # avg of avg TF in each doc
