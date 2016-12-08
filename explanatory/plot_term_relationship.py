3a464f2645960e2106e78a893fd215f9d34958ba
Peilin Yang <yangpeilyn@gmail.com>
2016-12-08 14:10:28 -0500

    add rel tf relationship plots

--- explanatory/plot_term_relationship.py
+++ explanatory/plot_term_relationship.py
@@ -303,7 +303,7 @@ class PlotTermRelationship(object):
             # # Mask zeros
             # Hmasked = np.ma.masked_where(H==0,H) # Mask pixels with a value
             #ax.pcolormesh(xedges,yedges,Hmasked)
-            ax.scatter(xaxis_plot, yaxis_plot, s=sizes)
+            ax.scatter(xaxis_plot, yaxis_plot, c=sizes)
             legend = 'AP(BM25):%.4f\n' % (float(rel_data[qid]['AP']['okapi'][1]))
             legend += '\n'.join(['%s:%.2f' % (ele[0], ele[1]) for ele in zip(terms, idfs)])
             ax.plot([0, max_value], [0, max_value], ls="dotted", label=legend)
