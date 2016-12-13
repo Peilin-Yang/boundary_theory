8b283e5cd896bfe3279246686d1c241608cc0e1f
Peilin Yang <yangpeilyn@gmail.com>
2016-12-13 11:40:03 -0500

    print ranking list directly from doc details

--- explanatory/plot_term_relationship.py
+++ explanatory/plot_term_relationship.py
@@ -387,7 +387,7 @@ class PlotTermRelationship(object):
                         float(rel_data[qid]['AP'][model_name][2].split(':')[1]), which_term=term_idx)
                     partial_order_index = np.argsort(partial_ranking_list)[::-1] # sort reversely
                     print qid, model_name, terms[term_idx-1], self.cal_map(all_rels[partial_order_index], rel_data[qid]['rel_cnt']) 
-                this_plot, = ax.plot(model_topranked_tfs[0], model_topranked_tfs[1], marker, alpha=0.3, label='%s:%.4f' % (model_name, float(rel_data[qid]['AP'][model_name][1])))
+                this_plot, = ax.plot(model_topranked_tfs[0], model_topranked_tfs[1], marker, alpha=0.3, label='%s:%.2f' % (model_name, float(rel_data[qid]['AP'][model_name][1])))
                 legend_handlers[this_plot] = HandlerLine2D(numpoints=1)
 
             ax.plot([0, max_value], [0, max_value], ls="dotted")
@@ -398,7 +398,7 @@ class PlotTermRelationship(object):
             ax.set_ylim([0, max_value])
             ax.grid(ls='dotted')
             #ax.ticklabel_format(axis='x', style='sci', scilimits=(0,0))
-            ax.legend(handler_map=legend_handlers, loc='best', fontsize=8)
+            ax.legend(handler_map=legend_handlers, loc='best', fontsize=6, markerscale=0.6)
 
         output_fn = os.path.join(self.output_root, '%s-%d-tf_relation.%s' % (self.collection_name, query_length, oformat) )
         plt.savefig(output_fn, format=oformat, bbox_inches='tight', dpi=400)
