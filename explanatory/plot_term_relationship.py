620cbe546b8c20a637ea5124754d290fc9152f80
Peilin Yang <yangpeilyn@gmail.com>
2016-12-13 14:27:23 -0500

    change xlabel

--- explanatory/plot_term_relationship.py
+++ explanatory/plot_term_relationship.py
@@ -320,9 +320,8 @@ class PlotTermRelationship(object):
         num_cols = min(4, len(details_rel_data)+1) # extra one for explanations
         num_rows = int(math.ceil((len(details_rel_data)+1)*1.0/num_cols))
         fig, axs = plt.subplots(nrows=num_rows, ncols=num_cols, sharex=False, sharey=False, figsize=(3*num_cols, 3*num_rows))
-        font = {'size' : 8}
-        plt.rc('font', **font)
-        matplotlib.rcParams['text.usetex'] = True
+        plt.rc('font', size=8)
+        plt.rc('text', usetex=True)
         row_idx = 0
         col_idx = 0
         for qid in sorted(details_rel_data):
