cdd43f19da7e0ad2067a11a82fcc0e7d89e71cd7
Peilin Yang <yangpeilyn@gmail.com>
2016-12-13 11:55:04 -0500

    print ranking list directly from doc details

--- explanatory/plot_term_relationship.py
+++ explanatory/plot_term_relationship.py
@@ -388,7 +388,10 @@ class PlotTermRelationship(object):
                         float(rel_data[qid]['AP'][model_name][2].split(':')[1]), which_term=term_idx)
                     partial_order_index = np.argsort(partial_ranking_list)[::-1] # sort reversely
                     partial_ranking_ap[term_idx-1] = self.cal_map(all_rels[partial_order_index], rel_data[qid]['rel_cnt']) 
-                this_plot, = ax.plot(model_topranked_tfs[0], model_topranked_tfs[1], marker, alpha=0.3, label='%s:%.3f(%.3f)' % (model_name, float(rel_data[qid]['AP'][model_name][1]), partial_ranking_ap[larger_idf_idx]))
+                this_plot, = ax.plot(model_topranked_tfs[0], model_topranked_tfs[1], marker, \
+                    alpha=0.3, label='%s:%.3f(%.3f)(%.3f)' % (model_name, \
+                        float(rel_data[qid]['AP'][model_name][1]), partial_ranking_ap[larger_idf_idx], \
+                        partial_ranking_ap[smaller_idf_idx]))
                 legend_handlers[this_plot] = HandlerLine2D(numpoints=1)
 
             ax.plot([0, max_value], [0, max_value], ls="dotted")
