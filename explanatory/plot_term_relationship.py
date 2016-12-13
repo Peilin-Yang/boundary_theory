0b14a1d808752e91a2a1efc4fc73be110e624811
Peilin Yang <yangpeilyn@gmail.com>
2016-12-13 11:14:51 -0500

    print ranking list directly from doc details

--- explanatory/plot_term_relationship.py
+++ explanatory/plot_term_relationship.py
@@ -349,9 +349,8 @@ class PlotTermRelationship(object):
             print query_length, dfs.size
             if query_length == 2 and dfs.size == 3:
                 terms = terms[1:]
-                dfs = dfs[1:]
                 tfs = tfs[1:]
-                print terms, tfs, dfs
+                dfs = dfs[1:]
             idfs = np.log((cs.get_doc_counts() + 1)/(dfs+1e-4))
             smaller_idf_idx = np.argmax(dfs)
             larger_idf_idx = np.argmin(dfs)
