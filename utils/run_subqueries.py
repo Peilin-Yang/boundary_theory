c1877c314b07bc0d6ae2fc0e1c7a0f34a9fb0923
Peilin Yang <yangpeilyn@gmail.com>
2016-12-19 16:30:34 -0500

    add run subqueries

--- utils/run_subqueries.py
+++ utils/run_subqueries.py
@@ -50,7 +50,7 @@ class RunSubqueries(object):
         if not os.path.exists(self.all_results_root):
             os.makedirs(self.all_results_root)
 
-        self.final_results_root = '../../all_results'
+        self.final_results_root = '../all_results'
         self.final_output_root = os.path.join(self.final_results_root, 'subqueries')
         if not os.path.exists(self.final_output_root):
             os.makedirs(self.final_output_root)
@@ -244,6 +244,8 @@ class RunSubqueries(object):
             all_data[-1].insert(0, qid)
             for model_para in model_paras:
                 all_data.append([subquery_data[qid][q][model_para] if model_para in subquery_data[qid][q] else '' for q in subqueries[1:]])
+                print all_data
+                raw_input()
                 all_data[-1].insert(0, model_para)
 
         with open(os.path.join(self.final_output_root, self.collection_name+'-'+str(query_length)+'.csv'), 'wb') as f:
