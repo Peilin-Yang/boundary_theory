517f903f188e6514fb6d9db3b03ffdec7a949062
Peilin Yang <yangpeilyn@gmail.com>
2017-02-13 14:02:02 -0500

    add run_batch para

--- utils/subqueries_learning.py
+++ utils/subqueries_learning.py
@@ -1209,7 +1209,7 @@ class SubqueriesLearning(RunSubqueries):
             for term in orig_query:
                 terms_stats[term] = cs.get_term_stats(term)
             all_runfiles = os.listdir(os.path.join(ele[0], 'subqueries', 'runfiles'))
-            print ele[1], qid, optimal_subquery_id, allterm_subquery_id
+            print ele[1], qid, orig_query, optimal_subquery_id, allterm_subquery_id
             optimal_subquery_runfile = [fn for fn in all_runfiles if fn.startswith(qid+'_'+optimal_subquery_id+'_method:okapi')][0]
             allterm_subquery_runfile = [fn for fn in all_runfiles if fn.startswith(qid+'_'+allterm_subquery_id+'_method:okapi')][0]
             
