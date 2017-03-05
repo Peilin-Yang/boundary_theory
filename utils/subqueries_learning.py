c60929741c6439e83f6d72116c222810a921bdff
Peilin Yang <yangpeilyn@gmail.com>
2017-03-05 16:53:56 -0500

    output feature files with integer labels

--- utils/subqueries_learning.py
+++ utils/subqueries_learning.py
@@ -1217,23 +1217,22 @@ class SubqueriesLearning(RunSubqueries):
             model_output_fn = os.path.join(results_root, 'model_%s_%d' 
                 % (test_collection, query_length) )
             if not os.path.exists(model_output_fn):
-                print model_output_fn
                 command = 'java -jar ~/Downloads/RankLib-2.8.jar -train %s -ranker 6 -save %s' % (trainging_fn, model_output_fn)
-                subprocess.call(command.split(), stdout=PIPE, stderr=PIPE)
-                #returncode = p.wait()
-                # out, error = p.communicate()
-                # if returncode != 0:
-                #     print "Run Query Error: %s %s" % (command, error)
+                p = Popen(command.split(), stdout=PIPE, stderr=PIPE)
+                returncode = p.wait()
+                out, error = p.communicate()
+                if returncode != 0:
+                    print "Run Query Error: %s %s" % (command, error)
 
             predict_fn = os.path.join(results_root, 'predict_%s_%d' 
                 % (test_collection, query_length))
             if not os.path.exists(predict_fn):
                 command = 'java -jar ~/Downloads/RankLib-2.8.jar -load %s -rank %s -score %s' % (model_output_fn, testing_fn, predict_fn)
-                subprocess.call(command.split(), stdout=PIPE, stderr=PIPE)
-                #returncode = p.wait()
-                # out, error = p.communicate()
-                # if returncode != 0:
-                #     raise NameError("Run Query Error: %s %s" % (command, error))
+                p = Popen(command.split(), stdout=PIPE, stderr=PIPE)
+                returncode = p.wait()
+                out, error = p.communicate()
+                if returncode != 0:
+                    raise NameError("Run Query Error: %s %s" % (command, error))
         elif method == 'svmrank':
             for c in range(-3, 5):
                 model_output_fn = os.path.join(results_root, 'model_%s_%d_%s' 
