7a832df1b6e796e842e59a8be2e7922d6b4eba85
Peilin Yang <yangpeilyn@gmail.com>
2017-03-06 14:05:02 -0500

    output corr features all collections

--- utils/subqueries_learning.py
+++ utils/subqueries_learning.py
@@ -1029,10 +1029,8 @@ class SubqueriesLearning(RunSubqueries):
             if returncode != 0:
                 raise NameError("Run Query Error: %s" % (command) )
             query_length = int(feature_fn.split('.')[0])
-            if method == 1:
-                err_rate = float(out.split('\n')[-2].split(':')[1])
-            elif method == 2:
-                err_rate = float(out.split('\n')[-4].split(':')[1])
+            err_rate = float(out.split('\n')[-2].split(':')[1])
+            if method == 2:
                 command = ['java -jar -Xmx2g ~/Downloads/RankLib-2.8.jar -load %s -rank %s -score %s'
                     % (os.path.join(model_root, fn), 
                     os.path.join(self.subqueries_features_root, folder, feature_fn),
