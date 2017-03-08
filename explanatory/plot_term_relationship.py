349efd49aa9b0c50f777892227652ed5463cb2ea
Peilin Yang <yangpeilyn@gmail.com>
2017-03-08 12:53:31 -0500

    reduce feature space

--- explanatory/plot_term_relationship.py
+++ explanatory/plot_term_relationship.py
@@ -364,6 +364,7 @@ class PlotTermRelationship(object):
             doclens = details_rel_data[qid][3]
             all_tfs = details_data[qid][1]
             print rel_tfs
+            raw_input()
             if method == 2: # BM25
                 okapi_optimal = Performances(self.collection_path).load_optimal_performance(['okapi'])[0]
                 okapi_para = 'method:%s,' % okapi_optimal[0] + okapi_optimal[2]
@@ -399,7 +400,6 @@ class PlotTermRelationship(object):
                 counts = prob_counts
             elif plot_option == 3:
                 counts = all_counts
-            print plot_option, counts
             xaxis_plot, yaxis_plot = zip(*counts.keys())
             sizes = np.array(counts.values())
             max_value = max(max(xaxis_plot), max(yaxis_plot))
