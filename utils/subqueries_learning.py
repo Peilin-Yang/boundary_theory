f960aaec0d7076e9e95461bc5c28cc9a1ac40952
Peilin Yang <yangpeilyn@gmail.com>
2017-03-08 13:07:07 -0500

    reduce feature space

--- utils/subqueries_learning.py
+++ utils/subqueries_learning.py
@@ -326,11 +326,13 @@ class SubqueriesLearning(RunSubqueries):
                 if orig_ranking_scores:
                     orig_features = self.get_all_sorts_features(orig_ranking_scores)
                 else:
-                    orig_features = [0, 0, 0, 0, 0, 0, 0, 0]
+                    # orig_features = [0, 0, 0, 0, 0, 0, 0, 0]
+                    orig_features = [0, 0, 0]
                 if prox_ranking_scores:
                     prox_features = self.get_all_sorts_features(prox_ranking_scores)
                 else:
-                    prox_features = [0, 0, 0, 0, 0, 0, 0, 0]
+                    # orig_features = [0, 0, 0, 0, 0, 0, 0, 0]
+                    orig_features = [0, 0, 0]
                 diff_features = np.array(prox_features) - np.array(orig_features)
                 features[subquery_id][name] = []
                 features[subquery_id][name].extend(orig_features)
