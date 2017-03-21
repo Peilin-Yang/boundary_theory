6292f3ad8e700abef0c7f57513b928ecdcae5d8d
Peilin Yang <yangpeilyn@gmail.com>
2017-03-21 17:07:11 -0400

    bug fix

--- utils/subqueries_learning.py
+++ utils/subqueries_learning.py
@@ -435,7 +435,7 @@ class SubqueriesLearning(RunSubqueries):
         if shape[1] == 2:
             return center[np.argmax(center)], points[np.argmax(center)]
         elif shape[1] == 3:
-            axes = [for i in range(3) if i != which_axis]
+            axes = [i for i in range(3) if i != which_axis]
             return np.linalg.norm(center[axes[0]], center[axes[1]]), np.apply_along_axis(self.cal_point_to_axis_distances3, 1, points, axes)
 
 
