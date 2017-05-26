a472d133a6b27693e4642cf502730d51d70d7a94
Peilin Yang <yangpeilyn@gmail.com>
2017-05-26 14:04:52 -0400

    correct okapi

--- explanatory/curve_fitting.py
+++ explanatory/curve_fitting.py
@@ -44,6 +44,7 @@ class RealModels(object):
         idfs = np.log((collection_stats.get_doc_counts() + 1)/(df+1e-4))
         avdl = collection_stats.get_avdl()
         r1 = np.apply_along_axis(self.okapi_apply1, 0, tf, idfs, k1)
+        print doclen
         r2 = np.apply_along_axis(self.okapi_apply2, 0, tf, doclen, avdl, k1, b)
         print tf, doclen, avdl, r2
         return np.sum(r2, axis=0)
