54184d4cc860c99f7686aa39fc4bbdca1f23df8f
Peilin Yang <yangpeilyn@gmail.com>
2017-05-26 14:19:08 -0400

    correct okapi

--- explanatory/curve_fitting.py
+++ explanatory/curve_fitting.py
@@ -46,9 +46,9 @@ class RealModels(object):
         r1 = np.apply_along_axis(self.okapi_apply1, 0, tf, idfs, k1)
         r2 = k1*(1-b+b*doclen/avdl)
         r3 = np.apply_along_axis(self.okapi_apply2, 1, tf, r2)
-        print tf, doclen, avdl, r2, r3
+        print r1, r3, r1/r3
         exit()
-        return np.sum(r2, axis=0)
+        return np.sum(r1/r3, axis=0)
     def tf1(self, collection_stats, tf, df, doclen):
         """
         tf - numpy matrix (even if there is only one term), each row is the tf values for each term
