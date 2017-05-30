710247b8c5e9b564fdc4e63d18d168207c494bfe
Peilin Yang <yangpeilyn@gmail.com>
2017-05-30 16:15:42 -0400

    fix plot all

--- explanatory/plot_prob_rel.py
+++ explanatory/plot_prob_rel.py
@@ -247,7 +247,6 @@ class PlotRelProb(object):
                 yaxis = [x_dict[x][0]*1./x_dict[x][1] for x in xaxis]
             else:
                 yaxis = [(x_dict[x][0]) if plot_rel_or_all else (x_dict[x][1]) for x in xaxis]
-            print yaxis
             # ranking_list = [(x_dict[x][0], x_dict[x][1]) for x in xaxis]
             # print ranking_list
             # all_expected_maps.append(EMAP().cal_expected_map(ranking_list))
