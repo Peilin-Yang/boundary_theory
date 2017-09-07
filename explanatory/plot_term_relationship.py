c75864f9605ce6b76a3bf0a125ed95e0b84f5fe8
Peilin Yang <yangpeilyn@gmail.com>
2017-09-07 16:19:57 -0700

    output json data

--- explanatory/plot_term_relationship.py
+++ explanatory/plot_term_relationship.py
@@ -641,8 +641,8 @@ class PlotTermRelationship(object):
                 sizes = np.array(counts.values())
                 max_value = max(max(xaxis_plot), max(yaxis_plot))
                 json_output[qid][plot_option] = {
-                    'x': xaxis_plot[0].tolist, 
-                    'y': yaxis_plot[0].tolist,
+                    'x': xaxis_plot[0].tolist(), 
+                    'y': yaxis_plot[0].tolist(),
                     'size': sizes,
                     'max': max_value
                 }
